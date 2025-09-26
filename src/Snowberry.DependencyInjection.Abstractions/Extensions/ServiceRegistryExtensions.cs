8d6f18ac37fc25235efd5325b6a9b6537b188425
VNC <main@vnncc.dev>
2025-09-27 00:00:23 +0200

    refactor: remove unused using directives in ServiceRegistryExtensions

--- src/Snowberry.DependencyInjection.Abstractions/Extensions/ServiceRegistryExtensions.cs
+++ src/Snowberry.DependencyInjection.Abstractions/Extensions/ServiceRegistryExtensions.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Snowberry.DependencyInjection.Abstractions.Interfaces;
+﻿using Snowberry.DependencyInjection.Abstractions.Interfaces;
 
 namespace Snowberry.DependencyInjection.Abstractions.Extensions;
 
